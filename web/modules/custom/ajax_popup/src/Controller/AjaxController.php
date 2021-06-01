dbdb4d46bb7c14c599a15e6a5df8952dd62d57f9
AnOst <anost3@gmail.com>
2021-06-01 13:41:37 +0300

    Ajax popup

---
+++ web/modules/custom/ajax_popup/src/Controller/AjaxController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\ajax_popup\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\NodeInterface;
+
+
+
+class AjaxController extends ControllerBase
+{
+
+    public function index(NodeInterface $node)
+    {
+         $query = \Drupal::database()->select('node_field_data', 'nfd');
+        $query->fields('nfd', ['nid', 'title']);
+        $query->condition('nfd.type', 'article');
+        $list = $query->countQuery()->execute()->fetchField();
+        
+        $output = sprintf(
+            '<div 
+        class="node-short-info">
+        <div class="node-short-info-title">Count node:%s</div>
+        </div>',
+            $list,
+            $node->getOwner()->getDisplayName(),
+        );
+        $render = [
+            '#type' => 'inline_template',
+            '#template' => $output,
+        ];
+        return $render;
+    }
+}
