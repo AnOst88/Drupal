e8176e5f5bb6c700781cb4dab70214f85ce3bb26
AnOst <anost3@gmail.com>
2021-06-01 17:14:19 +0300

    Prepared templates

--- web/modules/custom/ajax_popup/src/Controller/AjaxController.php
+++ web/modules/custom/ajax_popup/src/Controller/AjaxController.php
@@ -10,19 +10,19 @@ use Drupal\node\NodeInterface;
  */
 class AjaxController extends ControllerBase {
 
-public function index(NodeInterface $node) {
-    $query = \Drupal::database()->select('node_field_data', 'nfd');
-    $query->fields('nfd', ['nid', 'title']);
-    $query->condition('nfd.type', 'article');
-    $list = $query->countQuery()->execute()->fetchField();
-    $output = sprintf(
-        '<div class="node-short-info"><div class="node-short-info-title">Count node:%s</div></div>',
-        $list,
-    );
-    $render = [
-        '#type' => 'inline_template',
-        '#template' => $output,
-    ];
-    return $render;
+    public function index(NodeInterface $node) {
+        $query = \Drupal::database()->select('node_field_data', 'nfd');
+        $query->fields('nfd', ['nid', 'title']);
+        $query->condition('nfd.type', 'article');
+        $list = $query->countQuery()->execute()->fetchField();
+        $output = sprintf(
+            $list,
+        );
+        $render = [
+            '#theme' => 'ajax_popup',
+            '#var' => $list,
+            '#type' => 'inline_template',
+        ];
+        return $render;
     }
 }
