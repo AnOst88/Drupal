d2676b4e8af8ce90381411daafbf4e97a18c9f4f
AnOst <anost3@gmail.com>
2021-06-01 17:37:58 +0300

    Coding standarts

--- web/modules/custom/ajax_popup/src/Controller/AjaxController.php
+++ web/modules/custom/ajax_popup/src/Controller/AjaxController.php
@@ -10,19 +10,19 @@ use Drupal\node\NodeInterface;
  */
 class AjaxController extends ControllerBase {
 
-    public function index(NodeInterface $node) {
-        $query = \Drupal::database()->select('node_field_data', 'nfd');
-        $query->fields('nfd', ['nid', 'title']);
-        $query->condition('nfd.type', 'article');
-        $list = $query->countQuery()->execute()->fetchField();
-        $output = sprintf(
-            $list,
-        );
-        $render = [
-            '#theme' => 'ajax_popup',
-            '#var' => $list,
-            '#type' => 'inline_template',
-        ];
-        return $render;
+  public function index(NodeInterface $node) {
+    $query = \Drupal::database()->select('node_field_data', 'nfd');
+    $query->fields('nfd', ['nid', 'title']);
+    $query->condition('nfd.type', 'article');
+    $list = $query->countQuery()->execute()->fetchField();
+    $output = sprintf(
+      $list,
+      );
+    $render = [
+      '#theme' => 'ajax_popup',
+      '#count_node' => $list,
+      '#type' => 'inline_template',
+      ];
+    return $render;
     }
 }
