b4f7a7efbddd033f13a6e6903c1d0f271df901a7
AnOst <anost3@gmail.com>
2021-07-16 09:42:15 +0300

    CSV import

---
+++ web/modules/custom/custom_csv_import/src/CSVBatchImport.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Drupal\custom_csv_import;
+
+use Drupal\file\Entity\File;
+use Drupal\user\Entity\User;
+
+/**
+ * Class CSVBatchImport.
+ *
+ * @package Drupal\custom_csv_import
+ */
+class CSVBatchImport {
+
+  private $batch;
+
+  private $fid;
+
+  private $file;
+
+  private $skip_first_line;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($fid, $skip_first_line = FALSE, $batch_name = 'Custom CSV import') {
+    $this->fid = $fid;
+    $this->file = File::load($fid);
+    $this->skip_first_line = $skip_first_line;
+    $this->batch = [
+      'title' => $batch_name,
+      'finished' => [$this, 'finished'],
+      'file' => drupal_get_path('module', 'custom_csv_import') . '/src/CSVBatchImport.php',
+    ];
+    $this->parseCSV();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function parseCSV() {
+    if (($handle = fopen($this->file->getFileUri(), 'r')) !== FALSE) {
+      if ($this->skip_first_line) {
+        fgetcsv($handle, 0, ';');
+      }
+      while (($data = fgetcsv($handle, 0, ';')) !== FALSE) {
+        $this->setOperation($data);
+      }
+      fclose($handle);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOperation($data) {
+    $this->batch['operations'][] = [[$this, 'processItem'], $data];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($name, $mail) {
+      $user = User::create([
+      'type'  => 'user',
+      'langcode' => 'ru',
+      'status' => 1,
+      'name' => $name,
+      'mail' => $mail,
+    ]);
+
+    $user->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setBatch() {
+    batch_set($this->batch);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processBatch() {
+    batch_process();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function finished($success, $results, $operations) {
+    if ($success) {
+      $message = \Drupal::translation()
+        ->formatPlural(count($results), 'One post processed.', '@count posts processed.');
+    }
+    else {
+      $message = t('Finished with an error.');
+    }
+    drupal_set_message($message);
+  }
+
+}
