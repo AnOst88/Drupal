ec569035fff90865627edb06b1f3930d543f5196
AnOst <anost3@gmail.com>
2021-07-29 09:30:33 +0300

    cache tags

--- web/modules/custom/ao_task78_test_cache/src/Controller/CacheController.php
+++ web/modules/custom/ao_task78_test_cache/src/Controller/CacheController.php
@@ -3,7 +3,8 @@
 namespace Drupal\ao_task78_test_cache\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use \Drupal\user\Entity\User;
+use Drupal\user\Entity\User;
+use Drupal\core\Cache\Cache;
 
 /**
  * Provides route responses for the Cache module.
@@ -14,20 +15,26 @@ class CacheController extends ControllerBase {
    * Cached user name.
    *
    */
-  
   public function getCachedUserName() {
-    // Get user name
     $user = User::load(\Drupal::currentUser()->id());
     $user_name = $user->get('name')->value;
-    // Get cached user name
-    $cache_user_name = \Drupal::cache()->get('cachedUser', TRUE);
+    $data = null;
+    $cache_user_name = \Drupal::cache()->get('cachedUser', $user_name, $data,  Cache::PERMANENT);
 
+    $cache_tags = [
+      'cachedUser:5',
+      'cachedUser:3',
+      'cachedUser:6',
+    ];
+    
+    $data = $cache_user_name->data;
     if (empty($cache_user_name)) {
-      // Cached user name
-      \Drupal::cache()->set('cachedUser', $user_name);
-      $cache_user_name = \Drupal::cache()->get('cachedUser', TRUE);
+     // \Drupal::cache()->set('cachedUser', $user_name);
+     \Drupal::cache()->set('cachedUser', $user_name,  Cache::PERMANENT,  $cache_tags);
+     $t = \Drupal\Core\Entity\EntityTypeInterface::getListCacheTags();
+     dump($t);
+     $cache_user_name = \Drupal::cache()->get($user_name, TRUE);
     } 
-    // Return name from page
     return [
       '#markup' => $this->t('<h2>' . 'Name from cache:  %name' . '</h2>', [
         '%name' =>  $cache_user_name->data, 
@@ -42,7 +49,7 @@ class CacheController extends ControllerBase {
    * Return useer name from cache.
    *
    */
-  public function showCacheUserName() {
+  public function showCacheUserName($user_name) {
     $cache = \Drupal::cache()->get('cachedUser', TRUE);
     return [
       '#markup' => $cache->data,
