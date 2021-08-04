78b6311e699d5f2bcf048129282f4cf2019188f0
AnOst88 <58889034+AnOst88@users.noreply.github.com>
2021-08-04 10:40:26 +0300

    Update TextFilter.php

--- web/modules/custom/ao_task113_text_filter/src/Plugin/Filter/TextFilter.php
+++ web/modules/custom/ao_task113_text_filter/src/Plugin/Filter/TextFilter.php
@@ -5,8 +5,6 @@ namespace Drupal\ao_task113_text_filter\Plugin\Filter;
 use Drupal\filter\Plugin\FilterBase;
 use Drupal\filter\FilterProcessResult;
 use Drupal\Core\Form\FormStateInterface;
-
-
 /**
  * Replaces small letter of the word to uppercase.
  * 
@@ -19,7 +17,6 @@ use Drupal\Core\Form\FormStateInterface;
  *   "search" = " ",
  *   }
  * )
- *  
  */
 class TextFilter extends FilterBase{
 
@@ -28,9 +25,20 @@ class TextFilter extends FilterBase{
    */
   public function process($text, $langcode) {
     $result = new FilterProcessResult($text);
-    $config_words = $this->settings['search'];
-    $replaceable_words = explode(',', $config_words);
-    $fixed_text = str_replace($replaceable_words, array_map('ucwords',$replaceable_words), $text);
+    $words = $this->settings['search'];
+    $replaceable_words = explode(' ', $words);
+   // dump(gettype( $replaceable_words));
+   //dump($replaceable_words);
+    $income_texts = explode(' ', $text);
+   //dump($income_texts);
+
+    foreach($income_texts as $key => $search){
+      if (in_array($income_texts[$key], $replaceable_words, true)) {
+        $income_texts[$key] = mb_convert_case($search, MB_CASE_TITLE, "UTF-8");
+      }
+    }
+    //dump($text);
+    $fixed_text = implode(' ', $income_texts);
     $result->setProcessedText($fixed_text);
    
     return $result;
