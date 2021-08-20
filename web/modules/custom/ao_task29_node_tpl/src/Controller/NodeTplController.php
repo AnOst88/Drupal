9d182f2211cba2f7882248c86e96c3b5530c4312
AnOst <anost3@gmail.com>
2021-08-20 11:13:42 +0300

    Delete block

--- web/modules/custom/ao_task29_node_tpl/src/Controller/NodeTplController.php
+++ web/modules/custom/ao_task29_node_tpl/src/Controller/NodeTplController.php
@@ -3,6 +3,7 @@
 namespace Drupal\ao_task29_node_tpl\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
 
 /**
  * Block output node from template.
@@ -13,9 +14,16 @@ class NodeTplController extends ControllerBase {
    * Get and show node from template.
    */
   public function NodeTpl() {
-    return [
-      '#type' => 'item',
-      '#markup' => t('Task 29 - Get and show node from template.')
+    $nids = \Drupal::entityQuery('node')->condition('type','article')->execute();
+    $date_formatter = \Drupal::service('date.formatter');
+    foreach ($nids as $nid) {
+      $data = Node::load($nid);
+    }
+    return [ 
+      '#theme' => 'node_tpl',
+      '#id' => $data->id(),
+      '#label' => $data->label(),
+      '#date' => $date_formatter->format(time()),
     ];
   }
 }
