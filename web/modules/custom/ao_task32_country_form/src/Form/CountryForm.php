6e3c2d6c79e248a8dadf32bf160e200d9e5c7d8c
AnOst <anost3@gmail.com>
2021-08-23 13:31:30 +0300

    Prepared taxonomy city select

---
+++ web/modules/custom/ao_task32_country_form/src/Form/CountryForm.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\ao_task32_country_form\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * contact form.
+ */
+class CountryForm extends FormBase {
+
+  /**
+   * Form ID.
+   */
+  public function getFormId() {
+    return 'country_form';
+  }
+
+  /**
+   * Build form.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $terms_city = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('city');
+    $terms_country = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('country');
+ 
+    foreach ($terms_country as $country) {
+      $term_country[] = [
+        'country' => $country->name,
+      ];
+    }
+    foreach ($terms_city as $city) {
+      $term_city[] = $city->name;
+    }
+    
+    $form['taxonomy_city'] = [
+      '#type' => 'select',
+      '#target_type' => 'taxonomy_term',
+      '#options' => $term_city,
+      '#title' => t('City'),
+    ];
+
+    $form['taxonomy_country'] = [
+      '#type' => 'select',
+      '#target_type' => 'taxonomy_term',
+      '#options' =>  $term_country,
+      '#title' => t('Country'),
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Push'),
+    ];
+
+    return $form;
+    }
+
+      /**
+     * Submit form.
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+     
+    }
+}
