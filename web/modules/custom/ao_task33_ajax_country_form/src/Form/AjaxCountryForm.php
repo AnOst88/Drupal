c0ece495ad9223b499224d624f5ddb0f0e817b98
AnOst <anost3@gmail.com>
2021-08-24 16:44:17 +0300

    Ajax country/city form

--- web/modules/custom/ao_task33_ajax_country_form/src/Form/AjaxCountryForm.php
+++ web/modules/custom/ao_task33_ajax_country_form/src/Form/AjaxCountryForm.php
@@ -4,9 +4,7 @@ namespace Drupal\ao_task33_ajax_country_form\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Ajax\AjaxResponse;
-use Drupal\Core\Ajax\ReplaceCommand;
-use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * contact form.
@@ -22,15 +20,30 @@ class AjaxCountryForm extends FormBase {
 
   /**
    * Build form.
+   * 
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $terms_country = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('country');
+    $get_country = $form_state->getValue("taxonomy_country");
+    $terms_city = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
+      ->getQuery()
+      ->condition('vid', 'city')
+      ->condition('field_city_country', $get_country)
+      ->execute();
     $country_options = [];
+    $city_options = [];
+
+    foreach ($terms_city as $city) {
+      $term = \Drupal::entityTypeManager()
+        ->getStorage('taxonomy_term')
+        ->load($city);
+      $city_options[$term->id()] = $term->getName();
+    }
 
     foreach ($terms_country as $country) {
       $country_options[$country->tid] = $country->name;
     }
-    
+ 
     $form['taxonomy_country'] = [
       '#type' => 'select',
       '#options' => $country_options,
@@ -38,15 +51,15 @@ class AjaxCountryForm extends FormBase {
       '#ajax' => [
         'callback' => '::myAjaxCallback',
         'event' => 'change',
-        'wrapper' => 'first',
+        'wrapper' => 'ajax_city',
       ],
     ];
     
     $form['taxonomy_city'] = [
       '#type' => 'select',
-      '#prefix' => '<div id="first">',
+      '#prefix' => '<div id="ajax_city">',
       '#suffix' => '</div>',
-      '#options' => [],
+      '#options' => $city_options,
       '#title' => $this->t('City'),
     ];
 
@@ -54,25 +67,13 @@ class AjaxCountryForm extends FormBase {
       '#type' => 'submit',
       '#value' => $this->t('Push'),
     ];
-
     return $form;
-    }
+  }
 
   /**
   * Ajax callback.
   */
   public function myAjaxCallback(array &$form, FormStateInterface $form_state) {
-    $terms_city = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('city');
-    $city_options = [];
-
-    foreach ($terms_city as $city) {
-      $city_options[$city->tid] = $city->name;
-    }
- 
-    $selctedOption = $form_state->getValue("taxonomy_country");
-    $form['taxonomy_city']['#options'] = $city_options[$selctedOption]; 
-      // dump($form['taxonomy_city']);
-      // die();
     return $form['taxonomy_city'];
   }
 
@@ -80,9 +81,15 @@ class AjaxCountryForm extends FormBase {
   * Submit form.
   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $context = $this->t('City - %city, country - %country.', [
-      '%city' => $form_state->getValue('taxonomy_city'),
-      '%country' => $form_state->getValue('taxonomy_country')
+    $get_option_city = $form_state->getValue('taxonomy_city');
+    $object_city = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($get_option_city);
+
+    $get_option_country = $form_state->getValue('taxonomy_country');
+    $object_country = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($get_option_country);
+
+    $context = $this->t('City - %t_city. Country - %t_country.', [
+      '%t_city' => $object_city->get('name')->value,
+      '%t_country' =>   $object_country->get('name')->value,
     ]);
     \Drupal::logger('ao_task33_ajax_country_form')->notice($context);
     $this->messenger()->addMessage($context);
